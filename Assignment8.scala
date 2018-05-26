106b38f20eb22f63ecb867b083b2650d8fa2fd7b
Anubhav Saxena <anubhavsaxena14992@gmail.com>
2018-05-27 00:07:16 +0530

    Update file

--- Assignment8.scala
+++ Assignment8.scala
@@ -1,21 +1,19 @@
 import util.control.Breaks._
 object Assignment8{
     def main(args: Array[String]){
-        var arr = new Array[Int](3);
-        arr(0) = 11; arr(1) = 11; arr(2) = 11; arr(3) = 11; arr(4) = 11; arr(5) = 11;
+        var arr = new Array[Int](6);
+        arr(0) = 11; arr(1) = 20; arr(2) = 30; arr(3) = 76; arr(4) = 89; arr(5) = 40;
         var i:Int = 0;
+        var temp:Int = 0;
         var n = 6;
-        var j = n-1;
         breakable
         {
-            for( i <- 0 to n/2) 
+            while(i<(n/2)) 
             {
-                j = j-1;
-                arr(i) = arr(n-i)
-                if(i>=j)
-                {
-                    break;
-                }   
+                temp = arr(i);
+                arr(i) = arr(n-i-1);
+                arr(n-i-1) = temp;
+                i = i+1; 
             }
         }
         printf("Reverse of Array"); 
